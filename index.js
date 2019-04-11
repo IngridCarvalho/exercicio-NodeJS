0533388c96fff072e3d5592a694a08b980aefd69
Ingrid Carvalho <ingridcarvalho_1007@hotmail.com>
2019-04-11 08:30:16 -0300

    Finalizado edição e exclusão e feito gravação no arquivo a cada adição

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var expressLayouts = require('express-ejs-layouts');
+var fs = require('fs');
 
 var app = express();
 
 var model_contatos = require('./models/contatos');
 var contatos = model_contatos();
+
+var cont = 0;
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
@@ -24,7 +27,7 @@ app.get('/contatos', function(req, res){
 });
 
 app.post('/novo-contato', function(req, res){
-	let id1 = contatos.length + 1;
+	let id1 = cont += 1;
 	let nome1 = req.body.nome;
 	let telefone1 = req.body.telefone;
 	let email1 = req.body.email;
@@ -35,6 +38,9 @@ app.post('/novo-contato', function(req, res){
 
 	let pessoa = {id: id1, nome: nome1, telefone: telefone1, email: email1, endInst: endInst1, endCob: endCob1, velocidade: velocidade1, valor: valor1};
 
+	fs.writeFile('log.txt', `Adicionou: ${pessoa.id} - ${pessoa.nome}\r\n`, {encoding: 'utf-8', flag: 'a'}, function(err){
+		if(err) throw err;
+	})
 	console.log(`Adicionou: ${pessoa.nome}`); //adicionar isso em um arquivo
 	contatos.push(pessoa);
 	res.redirect('/');
@@ -50,10 +56,26 @@ app.get('/contatos/editar/:id', function(req, res){
 		);
 });
 
-app.post('/contatos/editar', function(req, res){
+app.post('/contatos/editar/', function(req, res){
+	id = req.body.id;
+	let index = contatos.map(c => c.id).indexOf(parseInt(id));
 
-	let oldContact = contatos.find(contato => contato.id == req.body.id);
-	console.log(oldContact);
+	contatos[index].nome = req.body.nome;
+	contatos[index].telefone = req.body.telefone;
+	contatos[index].email = req.body.email;
+	contatos[index].endInst = req.body.endInst;
+	contatos[index].endCob = req.body.endCob;
+	contatos[index].velocidade = req.body.velocidade;
+	contatos[index].valor = req.body.valor;
+
+	res.redirect('/');
+});
+
+app.get('/contatos/excluir/:id', function(req, res){
+	id = req.params.id;
+	let index = contatos.map(c => c.id).indexOf(parseInt(id));
+
+	contatos.splice(index, 1);
 
 	res.redirect('/');
 });
